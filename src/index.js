87a08e1af8ec604a952edb4a32684ab7ef3ecba5
Victor <vctudo.27@gmail.com>
2023-06-12 20:07:14 -0300

    ✨/💄term validation, error handling, ui term update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const lookupDetails = document.querySelector("#lookup-details");
+const lookupTerm = document.querySelector("#lookup-term");
 const tableDetails = document.querySelector("#table-details");
 const formattedTableLabels = [
   "MAC Address Prefix",
@@ -22,32 +23,50 @@ function initialPageState() {
 }
 
 function getMacDetails(event, macAddress) {
-  const fetchOptions = {
-    url: modalForm.action + `?query=${macAddress}`,
-    method: modalForm.method,
-    headers: {
-      "X-RapidAPI-Key": "816afd6ae1msh23a4b2de5529b26p1c58e4jsn2708b2358c88",
-      "X-RapidAPI-Host": "mac-address-lookup1.p.rapidapi.com",
-    },
-  };
-
   event.preventDefault();
-  fetch(fetchOptions.url, fetchOptions)
-    .then((resp) => resp.json())
-    .then(render)
-    .catch(onError);
+  let regexPattern = /^[0-9A-F]+$/;
 
-  closeModal(event);
+  if (macAddress.length < 12 || !regexPattern.test(macAddress)) {
+    alert("Invalid MAC address. Check your term and try again");
+  } else {
+    const fetchOptions = {
+      url: modalForm.action + `?query=${macAddress}`,
+      method: modalForm.method,
+      headers: {
+        "X-RapidAPI-Key": "911aaa2ab6msh0130883ff84ee8dp163772jsn45d7f2d88fe1",
+        "X-RapidAPI-Host": "mac-address-lookup1.p.rapidapi.com",
+      },
+    };
+
+    fetch(fetchOptions.url, fetchOptions)
+      .then(async (response) => {
+        let content = await response.json();
+        return { response, content };
+      })
+      .then((response) => {
+        response["macAddress"] = macAddress;
+        return response;
+      })
+      .then(render)
+      .catch(onError);
+
+    closeModal(event);
+  }
 }
 
 function render(resp) {
   clearLookup();
+  renderLookupTerm(resp);
 
-  if (resp.errors.length > 0) {
+  let status = resp.response.status;
+  let result = resp.content.result ?? [];
+  let errors = resp.content.errors ?? [];
+
+  if (status >= 400 || errors.length > 0) {
     throw resp;
   }
 
-  delete resp.result[0].b16;
+  delete result[0].b16;
 
   let containerDetails = document.createElement("section");
   containerDetails.classList.add("container-details");
@@ -59,7 +78,9 @@ function render(resp) {
 }
 
 function renderTable(resp) {
-  Object.entries(resp.result[0]).forEach((element, index) => {
+  let result = resp.content.result ?? [];
+
+  Object.entries(result[0]).forEach((element, index) => {
     let tr = document.createElement("tr");
     let key = document.createElement("td");
     let value = document.createElement("td");
@@ -74,6 +95,26 @@ function renderTable(resp) {
   });
 }
 
+function renderLookupTerm(resp) {
+  let macAddress = resp.macAddress;
+
+  let formattedMacAddress = "";
+  for (let i = 0; i <= 10; i = i + 2) {
+    formattedMacAddress += macAddress.slice(i, i + 2) + ":";
+  }
+
+  formattedMacAddress = formattedMacAddress.slice(
+    ":",
+    formattedMacAddress.lastIndexOf(":")
+  );
+
+  let newLookupTerm = document.createElement("h2");
+  newLookupTerm.innerHTML = formattedMacAddress;
+
+  lookupTerm.lastElementChild.remove();
+  lookupTerm.appendChild(newLookupTerm);
+}
+
 function clearLookup() {
   lookupDetails.firstElementChild.remove();
   tableDetails.replaceChildren("");
@@ -82,11 +123,29 @@ function clearLookup() {
 function onError(resp) {
   let errorElement = document.createElement("p");
 
-  resp.errors[0] == "Record Not Found."
-    ? (errorElement.innerHTML =
-        "We could not found a MAC Address within this range. Check the typed term and try again")
-    : (errorElement.innerHTML =
-        "Unfortunately, an error has ocurred. Please try again later.");
+  let errors = resp.content.errors ?? [];
+  let status = resp.response.status;
+
+  console.log(resp);
+
+  if (errors) {
+    switch (errors[0]) {
+      case "Record Not Found.":
+        errorElement.innerHTML =
+          "We could not found a MAC Address within this range. Check the typed term and try again";
+        break;
+      default:
+        errorElement.innerHTML =
+          "Unfortunately, an error has ocurred. Please try again later.";
+    }
+  } else {
+    status == 429
+      ? (errorElement.innerHTML =
+          "The maximum quota for this service has exceeded. Please try again tomorrow.")
+      : (errorElement.innerHTML =
+          "Unfortunately, an error has ocurred. Please try again later.");
+  }
+
   lookupDetails.appendChild(errorElement);
 }
 
